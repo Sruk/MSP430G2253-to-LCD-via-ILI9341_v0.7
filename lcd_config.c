6ee1b42ae96960be1aff337cb14051fa47bd4af6
Mario <flash.mario@gmail.com>
2014-03-10 22:50:43 +0100

    Update lcd_config.c

--- lcd_config.c
+++ lcd_config.c
@@ -18,81 +18,81 @@ typedef unsigned int uint16;
 
 #define DSPLY_OFF 	0x28
 #define DSPLY_ON 	0x29
-#define INVERT 		0					// color negative: enable 1, disable 0
+#define INVERT 		0			// color negative: enable 1, disable 0
 
 void init_ILI9340C(void)
 {
-	 	 	write_cmd_lcd(0x11);    	//Enter Sleep
+	write_cmd_lcd(0x11)		   	//Enter Sleep
 
-	 	 	timer_wait(100);
+	timer_wait(100);
 
-			write_cmd_lcd(0xCB);		// Power Control A
+	write_cmd_lcd(0xCB);			// Power Control A
 	        write_data_lcd(0x39);
 	        write_data_lcd(0x2C);
 	        write_data_lcd(0x00);
 	        write_data_lcd(0x34);		// VCORE: min 0x30, max 0x35
 	        write_data_lcd(0x02);		// VBC voltage: min 0x07, max 0x00
 
-	        write_cmd_lcd(0xCF);		// Power Control B
+	write_cmd_lcd(0xCF);			// Power Control B
 	        write_data_lcd(0x00);
 	        write_data_lcd(0X81);		// power-saving -> 0xC1
 	        write_data_lcd(0X30);
 
-	        write_cmd_lcd(0xE8);		// Driver Timing Ctrl A
+	write_cmd_lcd(0xE8);			// Driver Timing Ctrl A
 	        write_data_lcd(0x85);		// overlaping TFT gate time: normal 0x84, extended 0x85
 	        write_data_lcd(0x00);		// EQ time of gate driving: normal 0x00, extended 0x11
 	        write_data_lcd(0x78);		// precharge time: normal 0x7A, long 0x79, longest 0x78
 
-	        write_cmd_lcd(0xEA);		// Driver Timing Ctrl B
+	write_cmd_lcd(0xEA);			// Driver Timing Ctrl B
 	        write_data_lcd(0x00);
 	        write_data_lcd(0x00);
 
-	        write_cmd_lcd(0xED);		// Power On Sequence CTRL
+	write_cmd_lcd(0xED);			// Power On Sequence CTRL
 	        write_data_lcd(0x55);		// soft start time in 2B 0x(I)(II) by values: longest 4, long 5, short 6, off 7
 	        write_data_lcd(0x03);		// frame enable in 2B 0x(I)(II) by values: 0, 1, 2, 3
 	        write_data_lcd(0X12);		// frame enable in 2B 0x(I)(II) by values: 0, 1, 2, 3
 	        write_data_lcd(0X81);		// DVDDH enhance: disable 0x01, enable 0x81
 
-	        write_cmd_lcd(0xF7);		// Pump Ratio CTRL
+	write_cmd_lcd(0xF7);			// Pump Ratio CTRL
 	        write_data_lcd(0x20);		// DVDDH voltage multiplier: times two 0x20, times three 3 0x30
 
-	        write_cmd_lcd(0xC0);		//Power control 1
+	write_cmd_lcd(0xC0);			//Power control 1
 	        write_data_lcd(0x23);   	//VRH[5:0] -> greyscale value: max 0x3F, min 0x03
 
-	        write_cmd_lcd(0xC1);    	//Power control 2 (step-up voltage generator)
+	write_cmd_lcd(0xC1);    		//Power control 2 (step-up voltage generator)
 	        write_data_lcd(0x10);   	//SAP[2:0];BT[3:0]
 
-	        write_cmd_lcd(0xC5);    	//VCM control
+	write_cmd_lcd(0xC5);    		//VCM control
 	        write_data_lcd(0x3e);   	// --
 	        write_data_lcd(0x28);
 
-	        write_cmd_lcd(0xC7);    	//VCM control2
+	write_cmd_lcd(0xC7);    		//VCM control2
 	        write_data_lcd(0x86);  		// --
 
-	        write_cmd_lcd(0x36);    	// Memory Access Control (refresh order)
+	write_cmd_lcd(0x36);    		// Memory Access Control (refresh order)
 	        write_data_lcd(0xD8); 		//
 
-	        write_cmd_lcd(0x3A);		// Pixel Format
+	write_cmd_lcd(0x3A);			// Pixel Format
 	        write_data_lcd(0x55);		// 16bit 0x55, 18bit 0x66
 
-	        write_cmd_lcd(0xB1);		// Frame Rate CTRL
+	write_cmd_lcd(0xB1);			// Frame Rate CTRL
 	        write_data_lcd(0x00);		//
 	        write_data_lcd(0x1B);		// 199Hz max 0x10, 70Hz 0x1B, min 61Hz 0x1F
 
-	        color_invert();				// Color Inversion CTRL
+	 color_invert();			// Color Inversion CTRL
 
-	        write_cmd_lcd(0xB6);    	// Display Function Control (ILI3941 datasheet pg164)
+	 write_cmd_lcd(0xB6);    		// Display Function Control (ILI3941 datasheet pg164)
 	        write_data_lcd(0x0A);		//
 	        write_data_lcd(0x82);
 	        write_data_lcd(0x27);
 
-	        set_gamma();
+	  set_gamma();
 
-	        write_cmd_lcd(0x11);    	//Exit Sleep
-	        timer_wait(120);
+	  write_cmd_lcd(0x11);    		//Exit Sleep
+	  timer_wait(120);
 
-	        write_cmd_lcd(DSPLY_ON);    //Display on
-	        write_cmd_lcd(0x13);		// Normal mode
+	  write_cmd_lcd(DSPLY_ON);    		//Display on
+	  write_cmd_lcd(0x13);			// Normal mode
 
 }
 
@@ -107,44 +107,44 @@ void color_invert(void){
 
 void set_gamma(void){
 
-    write_cmd_lcd(0x26);    			//Gamma curve selected
-    write_data_lcd(0x01);				// ostale opcije: 0x02, 0x04, 0x08
+	write_cmd_lcd(0x26);    		//Gamma curve selected
+		write_data_lcd(0x01);		// other opt: 0x02, 0x04, 0x08
 
-    write_cmd_lcd(0xF2);    			// 3Gamma Function Disable
-    write_data_lcd(0x02);   			// disable: 0x02, enable: 0x03
+	write_cmd_lcd(0xF2);    		// 3Gamma Function Disable
+		write_data_lcd(0x02);   	// disable: 0x02, enable: 0x03
 
-    write_cmd_lcd(0xE0);    			//Set Positive Gamma
-    write_data_lcd(0x0F);
-    write_data_lcd(0x31);
-    write_data_lcd(0x2B);
-    write_data_lcd(0x0C);
-    write_data_lcd(0x0E);
-    write_data_lcd(0x08);
-    write_data_lcd(0x4E);
-    write_data_lcd(0xF1);
-    write_data_lcd(0x37);
-    write_data_lcd(0x07);
-    write_data_lcd(0x10);
-    write_data_lcd(0x03);
-    write_data_lcd(0x0E);
-    write_data_lcd(0x09);
-    write_data_lcd(0x00);
+	write_cmd_lcd(0xE0);    		//Set Positive Gamma
+		write_data_lcd(0x0F);
+		write_data_lcd(0x31);
+		write_data_lcd(0x2B);
+		write_data_lcd(0x0C);
+		write_data_lcd(0x0E);
+		write_data_lcd(0x08);
+		write_data_lcd(0x4E);
+		write_data_lcd(0xF1);
+		write_data_lcd(0x37);
+		write_data_lcd(0x07);
+		write_data_lcd(0x10);
+		write_data_lcd(0x03);
+		write_data_lcd(0x0E);
+		write_data_lcd(0x09);
+		write_data_lcd(0x00);
 
-    write_cmd_lcd(0XE1);    			//Set Negative Gamma
-    write_data_lcd(0x00);
-    write_data_lcd(0x0E);
-    write_data_lcd(0x14);
-    write_data_lcd(0x03);
-    write_data_lcd(0x11);
-    write_data_lcd(0x07);
-    write_data_lcd(0x31);
-    write_data_lcd(0xC1);
-    write_data_lcd(0x48);
-    write_data_lcd(0x08);
-    write_data_lcd(0x0F);
-    write_data_lcd(0x0C);
-    write_data_lcd(0x31);
-    write_data_lcd(0x36);
-    write_data_lcd(0x0F);
+	write_cmd_lcd(0XE1);    		//Set Negative Gamma
+		write_data_lcd(0x00);
+		write_data_lcd(0x0E);
+		write_data_lcd(0x14);
+		write_data_lcd(0x03);
+		write_data_lcd(0x11);
+		write_data_lcd(0x07);
+		write_data_lcd(0x31);
+		write_data_lcd(0xC1);
+		write_data_lcd(0x48);
+		write_data_lcd(0x08);
+		write_data_lcd(0x0F);
+		write_data_lcd(0x0C);
+		write_data_lcd(0x31);
+		write_data_lcd(0x36);
+		write_data_lcd(0x0F);
 
 }
